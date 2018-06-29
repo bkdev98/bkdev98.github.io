906ed5252dc025413b62d5622a4fba87e3efdeec
Quốc Khánh <bkdev98@gmail.com>
2018-06-29 23:54:07 +0700

    fix icon url

--- html.js
+++ html.js
@@ -40,7 +40,7 @@ export default function HTML(props) {
         {meta.toComponent()}
 
         <link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' />
-        <link rel='shortcut icon' href='http://localhost:8000/icons/icon.png' />
+        <link rel='shortcut icon' href='https://khanhquoc.press/icons/icon.png' />
         {css}
       </head>
       <body className='container'>
